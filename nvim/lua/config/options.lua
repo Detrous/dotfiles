3bbae75f3683514803375444e914a7a070f242ce
Artem Slobodian <detrous@protonmail.com>
2026-05-14 15:32:31 +0300

    Nvim: enable exrc for project-local config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Load project-local .nvim.lua (e.g. per-repo Kotlin test tasks). Files are trusted on first use.
+vim.o.exrc = true
